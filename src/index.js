9ce5c8792e9c24b0c0cbf92f79a728ea5a32359c
Oleh Linskyi <118620479+OlehLinskyy@users.noreply.github.com>
2023-04-27 00:44:17 +0300

    update project

--- src/index.js
+++ src/index.js
@@ -128,12 +128,11 @@ window.addEventListener('scroll', () => {
         const { height: cardHeight } = document
           .querySelector('.gallery')
           .firstElementChild.getBoundingClientRect();
-        console.log(cardHeight);
         window.scrollBy({
           top: cardHeight * 2,
           behavior: 'smooth',
         });
       });
-    }
-  }
+    };
+  };
 });
